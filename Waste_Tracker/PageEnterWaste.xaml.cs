c8e59a0b88bc5a6fff187e60a4d5f2e47d214bc2
Kip Tatum <v-datatu@microsoft.com>
2018-03-29 10:24:42 -0700

    Add project files.

---
+++ Waste_Tracker/PageEnterWaste.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace Waste_Tracker
+{
+    /// <summary>
+    /// Interaction logic for PageEnterWaste.xaml
+    /// </summary>
+    public partial class PageEnterWaste : Page
+    {
+        //SandboxEntities sbe = new SandboxEntities();
+        //CollectionViewSource miViewSource;
+        public PageEnterWaste()
+        {
+            InitializeComponent();
+            SandboxDataSet ds = ((SandboxDataSet)(FindResource("sandboxDataSet")));
+            SandboxDataSetTableAdapters.WasteTrackerDBTableAdapter da = new SandboxDataSetTableAdapters.WasteTrackerDBTableAdapter();
+            da.Fill(ds.WasteTrackerDB);
+            CollectionViewSource miViewSource = ((CollectionViewSource)(FindResource("wasteTrackerDBViewSource")));
+            
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            //sbe.WasteTrackerDBs.Load();
+            //miViewSource.Source = sbe.WasteTrackerDBs.Local;
+            
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SandboxDataSet ds = ((SandboxDataSet)(FindResource("sandboxDataSet")));
+            SandboxDataSetTableAdapters.WasteTrackerDBTableAdapter da = new SandboxDataSetTableAdapters.WasteTrackerDBTableAdapter();
+            da.Update(ds.WasteTrackerDB);
+            
+        }
+    }
+}
