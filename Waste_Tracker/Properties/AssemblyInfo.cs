88340558a5246878191c6630d6922b1b9402a44a
Kip Tatum <v-datatu@microsoft.com>
2018-05-30 07:37:46 -0700

    version update

--- Waste_Tracker/Properties/AssemblyInfo.cs
+++ Waste_Tracker/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Waste_Tracker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Waste and Par tracking application for SV Cafes")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Waste_Tracker")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.4")]
-[assembly: AssemblyFileVersion("1.1.1.4")]
+[assembly: AssemblyVersion("1.1.1.3")]
+[assembly: AssemblyFileVersion("1.1.1.3")]
